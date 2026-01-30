cf2c6a414d695f4f18b26267463a431c6ddd1711
kba1008 <sekolah-4896@moe-dl.edu.my>
2026-01-30 09:00:24 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,32 +1,23 @@
-// KEMASKINI VERSI DI SINI SETIAP KALI UBAH HTML
-const CACHE_NAME = 'ehadir-cache-v28'; 
-
+const CACHE_NAME = 'ehadir-cache-v27'; // Naikkan nombor ini setiap kali edit index.html
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json'
 ];
 
-// INSTALL: Simpan fail dalam cache
 self.addEventListener('install', event => {
-  self.skipWaiting(); // Paksa update segera
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// FETCH: Guna cache jika offline, update jika online
 self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request).catch(() => caches.match(event.request))
   );
 });
 
-// ACTIVATE: Buang cache lama (v19, v20, dll)
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
@@ -34,15 +25,10 @@ self.addEventListener('activate', event => {
       return Promise.all(
         cacheNames.map(cacheName => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
-            console.log('Buang cache lama:', cacheName);
             return caches.delete(cacheName);
           }
         })
       );
     })
   );
-  return self.clients.claim();
-
 });
-
-
