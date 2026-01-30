d0d61f4438a2a1217519af08f14c944892dd4a22
kba1008 <sekolah-4896@moe-dl.edu.my>
2026-01-30 10:12:00 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,35 +1,20 @@
-const CACHE_NAME = 'ehadir-cache-v31'; // Naikkan nombor ini setiap kali edit index.html
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+// sw.js - Versi 32
+const CACHE_NAME = 'ehadir-cache-v32'; 
+const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
 
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+  event.waitUntil(caches.keys().then(cacheNames => Promise.all(
+    cacheNames.map(cacheName => {
+      if (cacheName !== CACHE_NAME) return caches.delete(cacheName);
     })
-  );
-
+  )));
 });
